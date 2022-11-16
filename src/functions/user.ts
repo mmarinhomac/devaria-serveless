869dd03a858011653e1987d49c6d13b6adbc9f10
Matheus Marinho <matheus.marinho@manipulae.com.br>
2022-11-15 23:07:39 -0300

    endpoint search users

--- src/functions/user.ts
+++ src/functions/user.ts
@@ -1,4 +1,4 @@
-import { APIGatewayEvent, Context, Handler } from "aws-lambda";
+import { APIGatewayEvent, Handler } from "aws-lambda";
 import { parse } from "aws-multipart-parser";
 import { FileData } from "aws-multipart-parser/dist/models";
 
@@ -11,6 +11,7 @@ import {
 } from "../utils/formatResponseUtil";
 import { imageAllowedExtensions } from "../constants/regex";
 import { validateEnvs } from "../utils/environmentUtils";
+import { DefaultListPaginatedResponse } from "../types/DefaultListPaginatedResponse";
 
 export const me: Handler = async (
   event: APIGatewayEvent
@@ -103,5 +104,91 @@ export const update: Handler = async (
   } catch (e: any) {
     console.log("Error on register user: ", e);
     return formatDefaultResponse(500, "Erro ao atualizar usuário: " + e);
+  }
+};
+
+export const getById: Handler = async (
+  event: any
+): Promise<DefaultJsonResponse> => {
+  try {
+    const { AVATAR_BUCKET, error } = validateEnvs([
+      "USER_TABLE",
+      "AVATAR_BUCKET",
+    ]);
+    if (error) {
+      return formatDefaultResponse(500, error);
+    }
+
+    const { userId } = event.pathParameters;
+    if (!userId) {
+      return formatDefaultResponse(400, "Parâmetros de entrada não informados");
+    }
+
+    const user = await UserModel.get({ cognitoId: userId });
+    if (user && user.avatar) {
+      const url = await new S3Services().getImageUrl(
+        AVATAR_BUCKET,
+        user.avatar
+      );
+      user.avatar = url;
+    }
+
+    return formatDefaultResponse(200, null, user);
+  } catch (e) {
+    console.log("Error on get user data: ", e);
+    return formatDefaultResponse(500, "Erro ao buscar dados do usuário: " + e);
+  }
+};
+
+export const searchUser: Handler = async (
+  event: any
+): Promise<DefaultJsonResponse> => {
+  try {
+    const { AVATAR_BUCKET, error } = validateEnvs([
+      "USER_TABLE",
+      "AVATAR_BUCKET",
+    ]);
+    if (error) {
+      return formatDefaultResponse(500, error);
+    }
+
+    const { filter } = event.pathParameters;
+    const { lastKey } = event.queryStringParameters || "";
+    if (!filter || filter.length < 2) {
+      return formatDefaultResponse(400, "Parâmetros de entrada não informados");
+    }
+
+    const query = UserModel.scan()
+      .where("name")
+      .contains(filter)
+      .or()
+      .where("email")
+      .contains(filter);
+    if (lastKey) {
+      query.startAt({ cognitoId: lastKey });
+    }
+
+    const result = await query.limit(10).exec();
+    let response = {} as DefaultListPaginatedResponse;
+
+    if (result) {
+      response.count = result.count;
+      response.lastkey = result.lastKey;
+
+      for (const document of result) {
+        if (document && document.avatar) {
+          document.avatar = await new S3Services().getImageUrl(
+            AVATAR_BUCKET,
+            document.avatar
+          );
+        }
+      }
+      response.data = result;
+    }
+
+    return formatDefaultResponse(200, null, response);
+  } catch (e) {
+    console.log("Error on get users: ", e);
+    return formatDefaultResponse(500, "Erro ao buscar usuários: " + e);
   }
 };
